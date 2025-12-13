f3ac2ca1cd958ce4a74a502e3b2784d6473b53d2
Abhay Bansal <targeter001@gmail.com>
2025-12-13 19:53:24 +0530

    fix test issue while it is not running while declaring the sweetRoutes

--- BACKEND/src/app.ts
+++ BACKEND/src/app.ts
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(json());
 
 app.use("/api/auth", authRoutes); // <--- Add this line
-app.use("/api/sweets", sweetRoutes);
+// app.use("/api/sweets", sweetRoutes);
 // Placeholder for routes
 app.get("/", (req, res) => { res.send("API Running"); });
 
