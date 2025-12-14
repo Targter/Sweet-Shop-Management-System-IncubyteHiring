ee7b14d813c2caadcc64f456ed85f9bd0f4ea14b
Abhay Bansal <targeter001@gmail.com>
2025-12-14 06:45:41 +0530

    Fixed Backend Cors error

--- BACKEND/src/app.ts
+++ BACKEND/src/app.ts
@@ -5,10 +5,19 @@ import authRoutes from "./routes/authRoutes"; // <--- Import the auth routes
 import sweetRoutes from "./routes/sweetRoutes";
 
 const app = express();
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true
-}));
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//   credentials: true
+// }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(json());
 
 app.use("/api/auth", authRoutes); // <--- Add this line
