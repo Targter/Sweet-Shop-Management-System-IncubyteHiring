140048c7342293ebd963a5ea4876110945cf2f6c
Abhay Bansal <targeter001@gmail.com>
2025-12-14 06:39:34 +0530

    Cors error Fix

--- BACKEND/src/app.ts
+++ BACKEND/src/app.ts
@@ -5,7 +5,10 @@ import authRoutes from "./routes/authRoutes"; // <--- Import the auth routes
 import sweetRoutes from "./routes/sweetRoutes";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
+}));
 app.use(json());
 
 app.use("/api/auth", authRoutes); // <--- Add this line
