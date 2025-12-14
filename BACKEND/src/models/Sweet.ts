c0e16462e690dd01ceed740095b3b4e2f1e05e4a
Abhay Bansal <targeter001@gmail.com>
2025-12-14 10:21:43 +0530

    Refactor(backend) clean and improve the backend while adding the image url to the sweets schema and udpate whole test

--- BACKEND/src/models/Sweet.ts
+++ BACKEND/src/models/Sweet.ts
@@ -5,13 +5,18 @@ export interface ISweet extends Document {
   category: string;
   price: number;
   quantity: number;
+  image: string; // <--- Add this
 }
 
 const SweetSchema: Schema = new Schema({
   name: { type: String, required: true },
   category: { type: String, required: true },
   price: { type: Number, required: true },
-  quantity: { type: Number, required: true, min: 0 }
+  quantity: { type: Number, required: true, min: 0 }, image: { 
+    type: String, 
+    required: false, 
+    default: 'https://placehold.co/400x300?text=No+Image' 
+  }
 });
 
 export default mongoose.model<ISweet>('Sweet', SweetSchema);
